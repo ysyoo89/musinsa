5b83bcdcccd6ed6ad149ce3f65e92ac1dac4b0cb
ysyoo89 <ysyoo89@kakao.com>
2023-11-16 11:34:39 +0900

    Initial commit

---
+++ src/test/java/com/project/musinsa/MusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.musinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
